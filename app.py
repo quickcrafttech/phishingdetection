d46a940499c581031de18b10a7455abdd3e59280
naseralam <40887297+naserAlam@users.noreply.github.com>
2024-02-12 12:37:32 +0600

    download punkt and stopwords

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, render_template, redirect
 from model import Classifier
+from model_utils import Model_Utils
 
 app = Flask(__name__)
 
 model = Classifier()
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     prediction = None
-    if request.method == 'POST':
+    if request.method == "POST":
         # Get the text from the POST request
-        text = request.form['message']
+        text = request.form["message"]
 
         # Make prediction using the classifier
         result = model.predict(text)
 
-        if result==0:
-            prediction = 'Not Spam'
-        elif result==1:
-            prediction = 'Spam'
-        
-        return render_template('index.html', prediction=prediction)
+        if result == 0:
+            prediction = "Not Spam"
+        elif result == 1:
+            prediction = "Spam"
 
-    if request.method == 'GET':
-        return render_template('index.html', prediction=None)
+        return render_template("index.html", prediction=prediction)
+
+    if request.method == "GET":
+        return render_template("index.html", prediction=None)
+
 
 if __name__ == "__main__":
+    Model_Utils.download_lib()
     app.run(debug=True)
-    
--- model_utils.py
+++ model_utils.py
@@ -3,13 +3,28 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+import ssl
+
+
 class Model_Utils:
+    @staticmethod
+    def download_lib():
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context
+
+        nltk.download("stopwords")
+        nltk.download("punkt")
+
     @staticmethod
     def remove_punctuation_regex(text):
         # Define the regex pattern to match punctuation
-        pattern = r'[^\w\s]'  # Matches any character that is not a word character or whitespace
+        pattern = r"[^\w\s]"  # Matches any character that is not a word character or whitespace
         # Use the sub() function to replace punctuation with an empty string
-        text_no_punct = re.sub(pattern, '', text)
+        text_no_punct = re.sub(pattern, "", text)
         return text_no_punct
 
     @staticmethod
@@ -17,9 +32,9 @@ class Model_Utils:
         # Tokenize the text
         tokens = word_tokenize(text.lower())  # Convert to lowercase
         # Get English stopwords
-        stop_words = set(stopwords.words('english'))
+        stop_words = set(stopwords.words("english"))
         # Remove stopwords
         filtered_tokens = [word for word in tokens if word not in stop_words]
         # Join the tokens back into a string
-        clean_text = ' '.join(filtered_tokens)
+        clean_text = " ".join(filtered_tokens)
         return clean_text
